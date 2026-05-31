c468310a1edc62b796d2f81dcc76fab38cc36ff4
Sohamnayak4 <sohamnayak04@gmail.com>
2026-06-01 02:06:43 +0530

    vercel ana

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import { NotifyProvider } from "@/components/notify";
 
@@ -78,6 +79,7 @@ export default function RootLayout({
       </head>
       <body className="antialiased" suppressHydrationWarning>
         <NotifyProvider>{children}</NotifyProvider>
+        <Analytics />
       </body>
     </html>
   );
